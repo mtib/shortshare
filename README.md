f658a3d1271f38924ae3fc6cff59fe1abad2fdb4
Markus Becker <mtib.becker@gmail.com>
2020-10-06 12:04:20 +0200

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shortshare
 Easy sharing of data using compression and encoding.
+
+## binary
+This repository provides a single binary alongside the library functions, `go get github.com/mtib/shortshare`, then pipe into `shortshare` to generate shareable text, and pipe shared text into `shortshare -u` to decompress and decode.
+
+## library
+It's pretty easy to figure out ;)
--- share.go
+++ share.go
@@ -10,11 +10,13 @@ import (
 )
 
 type (
+	// Decoder only provided in case you prefer this calling scheme to the imperative Share()
 	Decoder struct {
 		reader io.Reader
 	}
 )
 
+// Share converts the provided data into a sharable (base64 urlencoded) bytes.Buffer, it uses the datas json representation as an intermediary.
 func Share(data interface{}) (*bytes.Buffer, error) {
 	b := new(bytes.Buffer)
 	cmpress, err := zlib.NewWriterLevel(base64.NewEncoder(base64.URLEncoding, b), zlib.BestCompression)
@@ -27,6 +29,7 @@ func Share(data interface{}) (*bytes.Buffer, error) {
 	return b, nil
 }
 
+// ShareString does the same as Share but converts the bytes.Buffer into a string.
 func ShareString(data interface{}) (string, error) {
 	r, err := Share(data)
 	if err != nil {
@@ -35,6 +38,7 @@ func ShareString(data interface{}) (string, error) {
 	return r.String(), nil
 }
 
+// Unshare unmarshals the data into the target.
 func Unshare(data io.Reader, target interface{}) error {
 	uncmpress, err := zlib.NewReader(base64.NewDecoder(base64.URLEncoding, data))
 	if err != nil {
@@ -45,16 +49,19 @@ func Unshare(data io.Reader, target interface{}) error {
 	return nil
 }
 
+// UnshareString unmarshals the data string into the target.
 func UnshareString(data string, target interface{}) error {
 	return Unshare(strings.NewReader(data), target)
 }
 
+// NewDecoder creates a new Decoder which reads from the io.Reader and offers a Decode(target) method.
 func NewDecoder(reader io.Reader) Decoder {
 	return Decoder{
 		reader,
 	}
 }
 
+// Decode decodes the data the reader this Decoder was created with has into the target.
 func (d Decoder) Decode(target interface{}) error {
 	return Unshare(d.reader, target)
 }
